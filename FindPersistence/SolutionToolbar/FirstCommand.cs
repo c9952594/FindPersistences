588088dce8c116bf065bb808d16fbbec73fc723b
Craig Norton <c9952594@gmail.com>
2016-12-22 00:30:45 +0000

    Added tutorial command to visual studio

---
+++ FindPersistence/SolutionToolbar/FirstCommand.cs
@@ -0,0 +1,115 @@
+﻿// Creating an Extension with a Menu Command
+// https://msdn.microsoft.com/en-us/library/cc138589.aspx
+
+// Adding a Command to the Solution Explorer Toolbar
+// https://msdn.microsoft.com/en-us/library/bb166773.aspx
+
+// In VS Solution Explorer, how to extend right click menus on source files sub code elements (class/method/field)
+// http://stackoverflow.com/questions/28696823/in-vs-solution-explorer-how-to-extend-right-click-menus-on-source-files-sub-cod
+
+//------------------------------------------------------------------------------
+// <copyright file="FirstCommand.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace SolutionToolbar {
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class FirstCommand {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("d4403ba1-e531-42e5-a505-7c6654096a92");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private FirstCommand(Package package) {
+            if (package == null) {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null) {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.StartNotepad, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        private void StartNotepad(object sender, EventArgs e) {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "notepad.exe";
+            proc.Start();
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static FirstCommand Instance {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider {
+            get {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package) {
+            Instance = new FirstCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e) {
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "FirstCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
